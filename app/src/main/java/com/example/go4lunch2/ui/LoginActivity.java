dd9185ddd8781dfcd07e7f5a441887d9b90884f4
Carolin <carochhu@gmail.com>
2022-06-10 11:47:11 +0200

    commit02: authentification

--- app/src/main/java/com/example/go4lunch2/ui/LoginActivity.java
+++ app/src/main/java/com/example/go4lunch2/ui/LoginActivity.java
@@ -2,15 +2,42 @@ package com.example.go4lunch2.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.firebase.ui.auth.AuthUI;
+
 import android.os.Bundle;
 
 import com.example.go4lunch2.R;
 
-public class LoginActivity extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+public class LoginActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        startSignInActivity();
+    }
+
+    private void startSignInActivity(){
+
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build(),
+                new AuthUI.IdpConfig.EmailBuilder().build());
+
+        // Launch the activity
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(providers)
+                        .setIsSmartLockEnabled(false, true)
+                        .setLogo(R.drawable.ic_logo)
+                        .build(),
+                RC_SIGN_IN);
     }
 }
--- app/src/main/java/com/example/go4lunch2/ui/MainActivity.java
+++ app/src/main/java/com/example/go4lunch2/ui/MainActivity.java
@@ -1,9 +1,10 @@
-package com.example.go4lunch2;
+package com.example.go4lunch2.ui;
 
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
 
+import com.example.go4lunch2.R;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
